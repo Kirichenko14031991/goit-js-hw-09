829fb322368b6d77c48e2a5e2d1e9ac8010043cf
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-07-07 21:28:37 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let intervalId = null;
 
+startButton.addEventListener('click', startColorSwitch);
+stopButton.addEventListener('click', stopColorSwitch);
+
+function startColorSwitch() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopColorSwitch() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor.padStart(6, '0')}`;
+}
