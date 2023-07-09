3a54c040f70a01c4f6862a5a977e3281710ac243
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2023-07-09 23:54:27 +0300

    3 after check

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,11 @@
-import Notiflix from "notiflix";
+import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
 const delay = document.querySelector('[name="delay"]');
 const amount = document.querySelector('[name="amount"]');
 const step = document.querySelector('[name="step"]');
+const submitButton = document.querySelector('[type="submit"]');
 
 form.addEventListener('submit', onSubmit);
 
@@ -28,14 +29,24 @@ function onSubmit(event) {
   let stepValue = Number(step.value);
   let delayValue = Number(delay.value);
 
+  submitButton.disabled = true;
+
+  const promises = [];
+
   for (let index = 1; index <= amountValue; index += 1) {
-    createPromise(index, delayValue)
+    const promise = createPromise(index, delayValue)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
+
+    promises.push(promise);
     delayValue += stepValue;
   }
+
+  Promise.all(promises).then(() => {
+    submitButton.disabled = false;
+  });
 }
